1dc857b714496abcfd48a658ede9d31bc9aaa9d0
Sneha Raj <sneharaj211979@gmail.com>
2026-02-14 20:08:25 +0530

    Update SignUp.js

--- FRONTEND/JavaScript/SignUp.js
+++ FRONTEND/JavaScript/SignUp.js
@@ -56,7 +56,7 @@ document.addEventListener("DOMContentLoaded", () => {
             signupBtn.textContent = 'Signing Up...';
 
             try {
-                const response = await fetch('https://unisync-sneha-raj05s-projects.vercel.app/api/auth/signup', {
+                const response = await fetch('https://uni-sync-sam7.vercel.app/api/auth/signup', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
@@ -131,3 +131,4 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
 });
+
