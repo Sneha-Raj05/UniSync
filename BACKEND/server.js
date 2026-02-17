898282623d9bd68f99a46d0d5545b5bd627c3257
Sneha Raj <sneharaj211979@gmail.com>
2026-02-17 23:46:37 +0530

    Update server.js

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(cors({
   origin: "https://uni-sync-iota.vercel.app", 
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
@@ -78,5 +78,6 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 export default app; // Sabse important line Vercel ke liye
+
 
 
